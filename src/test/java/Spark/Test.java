502541ebf88f2d6565894cd84df1df8e969efcd2
赵迎飞 <theshy6668@163.com>
2023-12-14 17:11:20 +0800

    2023-12-14 添加阿里 fastjson 依赖 - zyf

--- src/test/java/Spark/Test.java
+++ src/test/java/Spark/Test.java
@@ -1,11 +1,25 @@
 package Spark;
 
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.parser.Feature;
+
+import java.util.Iterator;
+import java.util.Map;
+
 /**
  * @author Malegod_xiaofei
  * @create 2023-11-30-15:28
  */
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello Word");
+        String s = "{\"品牌\":\"其他\"}";
+        Map baseInfo = JSONObject.parseObject(s, Feature.OrderedField);
+        Iterator<Map.Entry<String, String>> iter = baseInfo.entrySet().iterator();
+        while (iter.hasNext()) {
+            Map.Entry<String, String> entry = iter.next();
+            System.out.println(entry.getKey());
+            System.out.println(entry.getValue());
+        }
+
     }
 }
